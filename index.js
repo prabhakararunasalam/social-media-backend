c136ab5e38ce4c864a9fb2e8048f1f74348d0e7c
prabhakararunasalam <prabhakararunasalam17@gmail.com>
2025-01-21 11:02:49 +0530

    link updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://social-media-platform-frontend.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     }
